4d8223070501b7ed39e7e95641bc18d24f149302
Abigail Watson <abigailwatson@Abigails-Mac-mini-95666.local>
2018-03-10 20:57:23 -0600

    package.js

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'clinical:default-landing-page',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Default landing page for Meteor on FHIR Community.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/clinical-meteor/default-landing-page',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
